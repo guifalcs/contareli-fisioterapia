afa7456f2d098532d5e6b162f479f0f3d14821e0
Guilherme Falcão <guilherme.falcao@unvoid.com>
2025-03-25 15:41:37 -0300

    fix positions

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import { GoogleMap } from '@angular/google-maps';
+import { GoogleMap, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-maps',
-  imports: [GoogleMap],
+  imports: [GoogleMap, MapMarker],
   templateUrl: './maps.component.html',
-  styleUrl: './maps.component.scss'
+  styleUrl: './maps.component.scss',
 })
-export class MapsComponent {
-  options: google.maps.MapOptions = {
-    center: {lat: -19.50834073361128, lng: -42.57360633015932},
-    zoom: 4
-  };
-}
+export class MapsComponent {}
