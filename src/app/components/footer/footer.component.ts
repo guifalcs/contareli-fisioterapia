9c4e1807503093a61632dbc6f97dec302f446633
Guilherme Falcão <guilherme.falcao@unvoid.com>
2025-05-12 10:32:08 -0300

    Format texts

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -11,6 +11,8 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 export class FooterComponent {
   protected showLocation() {
     alert(`
+      Endereço:
+
       Av. Castelo Branco, 896 - Sala 606
       Horto, Ipatinga - MG
       CEP: 35160-294
@@ -19,17 +21,21 @@ export class FooterComponent {
 
   protected showContactInfo() {
     alert(`
-      (31) 99931-6529
-      contarelicentrodefisioterapia@gmail.com
+      Número: (31) 99931-6529
+
+      Email: contarelicentrodefisioterapia@gmail.com
       `);
   }
 
   protected showSchedule() {
     alert(`
+
+      Horários:
+
       Segunda à Sexta:
       07:00 - 19:00
       Sábado: 07:00 - 13:00
-      
+    
       *Mediante à agendamento prévio
       `);
   }
